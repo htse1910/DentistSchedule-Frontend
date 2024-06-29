addb8310b2613f78ede56a15ebe32fa3635538b9
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2024-06-30 06:30:51 +0700

    crud-deleteUser

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from '../axios';
 
 const handleLoginApi = (email, password) => {
-    return axios.post('/api/login', {email: email, password: password});
+    return axios.post('/api/login', { email: email, password: password });
 }
 const getAllUsers = (inputId) => {
     return axios.get(`/api/get-all-users?id=${inputId}`)
@@ -10,4 +10,12 @@ const createNewUserService = (data) => {
     console.log('check data from service: ', data)
     return axios.post('/api/create-new-user', data)
 }
-export {handleLoginApi , getAllUsers, createNewUserService}
+
+const deleteUserService = (userId) => {
+    return axios.delete('/api/delete-user', {
+        data: {
+            id: userId
+        }
+    });
+}
+export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService }
