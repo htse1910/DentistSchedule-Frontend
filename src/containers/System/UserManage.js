e7d281270a5a5205b2f5785afe52c6424a805e61
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2024-06-30 14:44:12 +0700

    crud-update-fe

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss'
-import { getAllUsers, createNewUserService, deleteUserService } from '../../services/userService';
+import { getAllUsers, createNewUserService, deleteUserService, editUserService } from '../../services/userService';
 import { get } from 'lodash';
 import ModalUser from './ModalUser';
+import ModalEditUser from './ModalEditUser';
 import { emitter } from "../../utils/emitter";
 class UserManage extends Component {
     constructor(props) {
@@ -12,12 +13,17 @@ class UserManage extends Component {
         this.state = {
             arrUsers: [],
             isOpenModalUser: false,
+            isOpenModalEditUser: false,
+            userEdit: {
+
+            }
         }
     }
 
     async componentDidMount() {
         await this.getAllUserFromReact();
     }
+
     getAllUserFromReact = async () => {
         let response = await getAllUsers('ALL');
         if (response && response.errCode === 0) {
@@ -34,7 +40,13 @@ class UserManage extends Component {
 
     toggleUserModal = () => {
         this.setState({
-            isOpenModalUser: !this.state.isOpenModalUser,
+            isOpenModalUser: !this.state.isOpenModalEditUser,
+        })
+    }
+
+    toggleUserEditModal = () => {
+        this.setState({
+            isOpenModalEditUser: !this.state.isOpenModalEditUser,
         })
     }
 
@@ -68,6 +80,32 @@ class UserManage extends Component {
         }
     }
 
+    handleEditUser = async (user) => {
+        console.log('check edit user ', user);
+        this.setState({
+            isOpenModalEditUser: true,
+            userEdit: user
+        })
+    }
+
+    doEditUser = async (user) => {
+        try {
+            let res = await editUserService(user);
+            if (res && res.errCode === 0) {
+                this.setState({
+                    isOpenModalEditUser: false
+                })
+                await this.getAllUserFromReact()
+            } else {
+                alert(res.errCode)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+
+        console.log('Click save user: ', user)
+    }
+
     render() {
         let arrUsers = this.state.arrUsers;
         return (
@@ -77,6 +115,15 @@ class UserManage extends Component {
                     toggleFromParent={this.toggleUserModal}
                     createNewUser={this.createNewUser}
                 />
+                {
+                    this.state.isOpenModalEditUser &&
+                    <ModalEditUser
+                        isOpen={this.state.isOpenModalEditUser}
+                        toggleFromParent={this.toggleUserEditModal}
+                        currentUser={this.state.userEdit}
+                        editUser={this.doEditUser}
+                    />
+                }
                 <div className="title text-center">Manage users with react</div>
                 <div className="mx-1">
                     <button
@@ -104,7 +151,9 @@ class UserManage extends Component {
                                         <td>{item.lastName}</td>
                                         <td>{item.address}</td>
                                         <td>
-                                            <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
+                                            <button className="btn-edit" onClick={
+                                                () => this.handleEditUser(item)
+                                            }><i className="fas fa-pencil-alt"></i></button>
                                             <button className="btn-delete" onClick={
                                                 () => this.handleDeleteUser(item)
                                             }><i className="fas fa-trash"></i></button>
