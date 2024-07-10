52d648ceeccedd5807b87165832906bdb8650e61
hoangduong1910 <hoangtrieuduong1910@gmail.com>
2024-07-10 16:37:33 +0700

    schedule css fix bug

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -118,13 +118,19 @@ class ManageSchedule extends Component {
             doctorId: selectedDoctor.value,
             formatedDate: formatedDate,
         })
-        console.log('Check res saveBulkScheduleDoctor ', res)
-        console.log('check result: ', result)
+        if (res && res.errCode === 0) {
+            toast.success("Save infor succeed!");
+        } else {
+            toast.error("Error saveBulkScheduleDoctor!");
+            console.log('Error saveBulkScheduleDoctor: ', res)
+        }
+        
     }
     
     render() {
         let { rangeTime } = this.state;
         let { language } = this.props;
+        let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
         return (
             <div className="manage-schedule-container">
                 <div className="m-s-title">
@@ -146,7 +152,7 @@ class ManageSchedule extends Component {
                                 onChange={this.handleOnchangeDatePicker}
                                 className="form-control"
                                 value={this.state.currentDate}
-                                minDate={new Date()}
+                                minDate = { yesterday }
                             />
                         </div>
                         <div className="col-12 pick-hour-container">
